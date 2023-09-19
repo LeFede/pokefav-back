581ad4d21e917955c6290621631a91d89d7cce6f
LeFede <lefedeok@gmail.com>
2023-09-19 18:55:26 -0300

    .env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,19 +1,18 @@
-import {initializeApp} from 'firebase/app';
-import {getAuth} from 'firebase/auth';
-import {getDatabase} from 'firebase/database';
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getDatabase } from "firebase/database"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBrPlfIdzj-YSXHh5stkzqU642RNa-xW5k',
-  authDomain: 'mobile-c2254.firebaseapp.com',
-  projectId: 'mobile-c2254',
-  storageBucket: 'mobile-c2254.appspot.com',
-  messagingSenderId: '237614622753',
-  appId: '1:237614622753:web:01838fb04d00e1e92cdb60',
-};
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+}
 
 // Initialize Firebase
-const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
-export const FIREBASE_DB = getDatabase();
-
+const FIREBASE_APP = initializeApp(firebaseConfig)
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
+export const FIREBASE_DB = getDatabase()
